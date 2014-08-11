fb789972b80abd9ef3029e7d9b335bef6d5b820e
Thomas Lagier <lagierta@gmail.com>
2014-08-10 22:33:57 -0700

    Initial commit

---
+++ Assets/Placeholder/Scripts/NoteBehaviorScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteBehaviorScript : MonoBehaviour {
+
+	//The time it takes for the note to move to the top of the screen
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.up * moveSpeed;
+	}
+}
